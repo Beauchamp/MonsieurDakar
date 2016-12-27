d5d075a72fc8c6638e61d614cfa0dd0783a03571
Plysovej Kaktus <filip.pokorny@solfa.uk>
2016-12-27 14:19:39 +0000

    script to transfer wheel rotation from unity wheel collider to a mesh, ready to attach to each wheel

---
+++ Assets/scripts/wheelColliderMesh.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class wheelColliderMesh : MonoBehaviour {
+
+	public WheelCollider Wheel;
+	Vector3 BaseEulers = Vector3.one;
+	Vector3 BaseTangent = Vector3.one;
+	float rotation_; 
+
+	void Start(){
+		if (BaseEulers == Vector3.one) {
+			BaseEulers = transform.localEulerAngles;
+			BaseTangent = new Vector3 (BaseEulers.x, BaseEulers.y, BaseEulers.z);
+		}
+	}
+
+	void Update () {
+		rotation_ += Wheel.rpm / 60 * 360 * Time.deltaTime;
+		transform.localEulerAngles = new Vector3(-rotation_, Wheel.steerAngle, 0) + BaseEulers;
+	}
+}
